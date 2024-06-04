3316b5e9789d57c55f0cf76552098d0a8e6a5c58
Azman_Ahmed <86096683+Azman-Ahmed@users.noreply.github.com>
2024-06-04 13:10:37 +0600

    Added Prime Factorization

--- Cp book.cpp
+++ Cp book.cpp
@@ -35,3 +35,19 @@ void sieve()
 
 //3. Prime Factorization
 
+void primeFact(int n)
+{
+    for(int i=2; i*i<=n; i++)
+    {
+        int c = 0;
+        while(n%i==0)
+        {
+            c++;
+            n/=i;
+        }
+        cout<<i<<" "<<c<<endl;
+    }
+    if(n>1) cout<<n<<" "<<1<<endl;
+} ///O(sqrt(n))
+
+
