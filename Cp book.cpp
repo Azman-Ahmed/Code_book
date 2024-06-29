9d41217f0aa15a6373a17e790d07a4e720514647
Azman Ahmed <ahmed.azman.bd0007@gmail.com>
2024-06-29 23:11:19 +0600

    Euler Totint Function Added

--- Cp book.cpp
+++ Cp book.cpp
@@ -103,6 +103,23 @@ int power(int a, int p, int m)
     return res;
 } //O(logn)
 
+///6. Euler Totient Function
+
+int phi(int n)
+{
+    int res = n;
+    for(int i = 2; i*i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            res/=i;
+            res*=(i-1);
+            while(n%i==0) n/=i;
+        }
+    }
+}
+
+
 
 
 
